1bc06a54b90509d68d90d499291300561117455e
Volodymyr Savchenko <vovasvch@gmail.com>
2021-01-22 22:50:59 +0200

    calculate neighbors coordinates + tests

--- test_generator.lua
+++ test_generator.lua
@@ -2,12 +2,111 @@ package.path = package.path .. ";.luamodules/share/lua/5.2/luaunit.lua"
 lu = require('luaunit')
 utils = require('utils')
 
-function testStub()
-    lu.assertEquals(0, 0)
+function testPoint()
+    point = utils.point(1, 2)
+    lu.assertEquals(point[0], 1)
+    lu.assertEquals(point[1], 2)
 end
 
-function testUnity()
-    lu.assertEquals(utils:unity(), 1)
+function testNeighborsCenterEven()
+    map = { height = 5, width = 5 } 
+    neighbors = utils.neighbors(2, 2, map)
+
+    lu.assertEquals(neighbors[0][0], 2)
+    lu.assertEquals(neighbors[0][1], 1)
+
+    lu.assertEquals(neighbors[1][0], 3)
+    lu.assertEquals(neighbors[1][1], 2)
+
+    lu.assertEquals(neighbors[2][0], 3)
+    lu.assertEquals(neighbors[2][1], 3)
+
+    lu.assertEquals(neighbors[3][0], 2)
+    lu.assertEquals(neighbors[3][1], 3)
+
+    lu.assertEquals(neighbors[4][0], 1)
+    lu.assertEquals(neighbors[4][1], 3)
+
+    lu.assertEquals(neighbors[5][0], 1)
+    lu.assertEquals(neighbors[5][1], 2)
+
+    lu.assertEquals(neighbors[6], nil)
+end
+
+function testNeighborsCenterOdd()
+    map = { height = 5, width = 5 } 
+    neighbors = utils.neighbors(3, 3, map)
+
+    lu.assertEquals(neighbors[0][0], 3)
+    lu.assertEquals(neighbors[0][1], 2)
+
+    lu.assertEquals(neighbors[1][0], 4)
+    lu.assertEquals(neighbors[1][1], 2)
+
+    lu.assertEquals(neighbors[2][0], 4)
+    lu.assertEquals(neighbors[2][1], 3)
+
+    lu.assertEquals(neighbors[3][0], 3)
+    lu.assertEquals(neighbors[3][1], 4)
+
+    lu.assertEquals(neighbors[4][0], 2)
+    lu.assertEquals(neighbors[4][1], 3)
+
+    lu.assertEquals(neighbors[5][0], 2)
+    lu.assertEquals(neighbors[5][1], 2)
+
+    lu.assertEquals(neighbors[6], nil)
+end
+
+function testNeighborsTopLeft()
+    map = { height = 5, width = 5 } 
+    neighbors = utils.neighbors(1, 1, map)
+
+    lu.assertEquals(neighbors[0][0], 2)
+    lu.assertEquals(neighbors[0][1], 1)
+
+    lu.assertEquals(neighbors[1][0], 1)
+    lu.assertEquals(neighbors[1][1], 2)
+
+    lu.assertEquals(neighbors[2], nil)
+end
+
+function testNeighborsBottomRight()
+    map = { height = 5, width = 5 } 
+    neighbors = utils.neighbors(5, 5, map)
+
+    lu.assertEquals(neighbors[0][0], 5)
+    lu.assertEquals(neighbors[0][1], 4)
+
+    lu.assertEquals(neighbors[1][0], 4)
+    lu.assertEquals(neighbors[1][1], 5)
+
+    lu.assertEquals(neighbors[2][0], 4)
+    lu.assertEquals(neighbors[2][1], 4)
+
+    lu.assertEquals(neighbors[3], nil)
+end
+
+function testNeighborsTopCenter()
+    map = { height = 5, width = 5 } 
+    neighbors = utils.neighbors(4, 1, map)
+
+    lu.assertEquals(neighbors[0][0], 5)
+    lu.assertEquals(neighbors[0][1], 1)
+
+    lu.assertEquals(neighbors[1][0], 5)
+    lu.assertEquals(neighbors[1][1], 2)
+
+    lu.assertEquals(neighbors[2][0], 4)
+    lu.assertEquals(neighbors[2][1], 2)
+
+    lu.assertEquals(neighbors[3][0], 3)
+    lu.assertEquals(neighbors[3][1], 2)
+
+    lu.assertEquals(neighbors[4][0], 3)
+    lu.assertEquals(neighbors[4][1], 1)
+
+    lu.assertEquals(neighbors[5], nil)
 end
 
 os.exit(lu.LuaUnit:run())
--- utils.lua
+++ utils.lua
@@ -1,7 +1,69 @@
 local utils = {}
 
-function utils:unity()
-    return 1
+function utils.point(x, y)
+    res = {}
+    res[0] = x
+    res[1] = y
+    return res
+end
+
+function utils.neighbors(x, y, map)
+    height = map["height"]
+    width = map["width"]
+    result = {}
+    i = 0
+
+    if (y - 1 > 0) then
+        result[i] = utils.point(x, y - 1)
+        i = i + 1
+    end
+
+    if (x + 1 <= width) then
+        if (x % 2 == 0) then
+            result[i] = utils.point(x + 1, y)
+            i = i + 1
+
+            if (y + 1 <= height) then
+                result[i] = utils.point(x + 1, y + 1)
+                i = i + 1
+            end
+        else
+            if(y - 1 > 0) then
+                result[i] = utils.point(x + 1, y - 1)
+                i = i + 1
+            end
+
+            result[i] = utils.point(x + 1, y)
+            i = i + 1
+        end
+    end
+
+    if (y + 1 <= height) then
+        result[i] = utils.point(x, y + 1)
+        i = i + 1
+    end
+
+    if (x - 1 > 0) then
+        if (x % 2 == 0) then
+            if (y + 1 <= height) then
+                result[i] = utils.point(x - 1, y + 1)
+                i = i + 1
+            end
+           
+            result[i] = utils.point(x - 1, y)
+            i = i + 1
+        else
+            result[i] = utils.point(x - 1, y)
+            i = i + 1
+
+            if (y - 1 > 0) then
+                result[i] = utils.point(x - 1, y - 1)
+                i = i + 1
+            end
+        end
+    end
+
+    return result
 end
 
 return utils

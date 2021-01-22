7fecd1f97088e5ece8867c9d5d43ca4f79927c44
Volodymyr Savchenko <vovasvch@gmail.com>
2021-01-22 16:35:31 +0200

    write_to_file function

--- generator.lua
+++ generator.lua
@@ -1,27 +1,38 @@
-file = io.open("test.map", "w+")
+function write_to_file(map)
+    file = io.open(map["name"] .. ".map", "w+")
 
-width = 6
-height = 6
+    width = map["width"]
+    height = map["height"]
 
-for i = 0, height + 1, 1 do
-    for j = 0, width + 1, 1 do
-        
-        if(    i == 0
-            or j == 0
-            or i == height + 1
-            or j == width + 1)
-        then
-            file:write("_off^_usr")
-        else
-            file:write("Gg")
+    for i = 0, height + 1, 1 do
+        for j = 0, width + 1, 1 do
+            
+            if(    i == 0
+                or j == 0
+                or i == height + 1
+                or j == width + 1)
+            then
+                file:write("_off^_usr")
+            else
+                if(math.random() > 0.95)
+                then
+                    file:write("Gg^Vh")
+                else
+                    file:write("Gg")
+                end
+            end
+
+            if (j < width + 1) then
+                file:write(", ")
+            end
         end
 
-        if (j < height + 1) then
-            file:write(", ")
-        end
+        file:write("\n")
     end
 
-    file:write("\n")
+    file:close()
 end
 
-file:close()
+map = { width = 20, height = 15, name = "test" }
+write_to_file(map)
+
